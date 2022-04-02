f3591d9ff46c8f495e02baa6a6c1ad9710290301
Nicolas Boisseau <48957756+Getiii@users.noreply.github.com>
2022-04-02 17:26:20 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,75 +1,135 @@
 # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import requests
+import json
+from json import JSONEncoder
+import pygame
 
-token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPVU5HQSBPVU5HQSIsImlzcyI6IlNwYWNlT2Rpc3NleUF1dGgifQ.OoCNHe710Cgg65ZdkpImaq0siT8n7pG5ByfKaga-2ro"
-url="http://37.187.28.218:8080"
-headers={'content-type': 'application/json','access-token': token}
 
-r = requests.get(url,headers=headers).json()
+
+myToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPVU5HQSBPVU5HQSIsImlzcyI6IlNwYWNlT2Rpc3NleUF1dGgifQ.OoCNHe710Cgg65ZdkpImaq0siT8n7pG5ByfKaga-2ro"
+url = "http://37.187.28.218:8080"
+head = {'content-type': 'application/json','access-token': myToken }
+teamName = "OUNGA OUNGA"
+
+
+# subclass JSONEncoder
+class setEncoder(JSONEncoder):
+    def default(self, obj):
+        return list(obj)
+    
 
 
 
 
 def GetShipID(shipNumber):
-    chemin="/team/OUNGA OUNGA/"
-    headers={'content-type': 'application/json','access-token': token}
-    r = requests.get(url+chemin,headers=headers).json()
-    
+    chemin="{}/team/OUNGA OUNGA/".format(url)
+    r = requests.get(chemin,headers=head).json()
     return r['ship_'+str(shipNumber)]['id']
     
     
     
 def GetMapID(mapNumber):
-    chemin="/map"
-    headers={'content-type': 'application/json','access-token': token}
-    r = requests.get(url+chemin,headers=headers).json()
-    
+    chemin="{}/map".format(url)
+    r = requests.get(chemin,headers=head).json()
     for i in r:
-        
         if mapNumber==i['mapNumber']:
             return i['id']
     
     
 def GetMap(mapNumber):
-    chemin="/map/"+GetMapID(mapNumber)+"/infos"
-    r = requests.get(url+chemin,headers=headers).json()
+    chemin="{}/map/{}/infos".format(url,GetMapID(1))
+    r = requests.get(chemin,headers=head).json()
     return r
 
 
 def GetExplorations(mapNumber):
-    chemin="/team/OUNGA OUNGA/map/"+GetMapID(mapNumber)+"/explorations"
-    r = requests.get(url+chemin,headers=headers).json()
+    chemin="/team/OUNGA OUNGA/map/{}/explorations".format(GetMapID(mapNumber))
+    r = requests.get(chemin,headers=head).json()
     return r
 
 def GetBestScore(mapNumber):
-    chemin="/team/OUNGA OUNGA/map/"+GetMapID(mapNumber)+"/explorations/max"
-    r = requests.get(url+chemin,headers=headers).json()
+    chemin="/team/OUNGA OUNGA/map/{}/explorations/max".format(GetMapID(mapNumber))
+    r = requests.get(chemin,headers=head).json()
     return r
 
 def GetShip(shipNumber):
     chemin="/team/OUNGA OUNGA/ships/"+GetShipID(shipNumber)
-    r = requests.get(url+chemin,headers=headers).json()
+    r = requests.get(chemin,headers=head).json()
     return r
 
 def GetShipCarto(shipNumber,mapNumber):
-    chemin="/team/OUNGA OUNGA/ships/"+GetShipID(shipNumber)+"/map/"+GetMapID(mapNumber)+"/carto"
-    r = requests.get(url+chemin,headers=headers).json()
+    chemin="{}/team/OUNGA OUNGA/ships/{}/map/{}/carto".format(url,GetShipID(shipNumber),GetMapID(mapNumber))
+    r = requests.get(chemin,headers=head).json()
     return r
 
 
 def StartVaisseau(shipNumber,mapNumber):
-    chemin="/team/OUNGA OUNGA/ships/"+GetShipID(shipNumber)+"/start/"+GetMapID(mapNumber)
-    r = requests.post(url+chemin,headers=headers)
+    chemin="/team/OUNGA OUNGA/ships/{}/start/{}".format(GetShipID(shipNumber),GetMapID(mapNumber))
+    r = requests.post(chemin,headers=head)
     return r
     
 
 
-#print(StartVaisseau(1,1))
-print(GetShipCarto(1,1))
+def action(shipID, x, y):
+    link = "{}/team/{}/ships/{}/actions".format(url,teamName,shipID)
+    aList = [{'actions':{'move:0,0'}}]
+    jsonData = setEncoder().encode(aList)
+    response = requests.put(link,headers = head, json = jsonData)
+    return response
 
+
+
+def startVaisseau(shipID, mapID):
+    headers = {
+        'access-token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPVU5HQSBPVU5HQSIsImlzcyI6IlNwYWNlT2Rpc3NleUF1dGgifQ.OoCNHe710Cgg65ZdkpImaq0siT8n7pG5ByfKaga-2ro'
+    }
+    
+    json_data = {
+        'actions': [
+            'move:0,0',
+        ],
+    }
+    
+    response = requests.post('{}/team/{}/ships/{}/start/{}'.format(url,teamName,shipID,mapID), headers=headers, json=json_data)
+    return response
+
+def stopVaisseau(shipID):
+    headers = {
+        'access-token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPVU5HQSBPVU5HQSIsImlzcyI6IlNwYWNlT2Rpc3NleUF1dGgifQ.OoCNHe710Cgg65ZdkpImaq0siT8n7pG5ByfKaga-2ro'
+    }
+    json_data = {
+        'actions': [
+            'move:0,0',
+        ],
+    }
+    response = requests.post('{}/team/{}/ships/{}/end/'.format(url,teamName,shipID), headers=headers, json=json_data)
+    return response
+
+
+print(startVaisseau(GetShipID(1), GetMapID(1)))
+r = GetShipCarto(1,1)
+
+for data in r:
+    print(data)
+#print(stopVaisseau(GetShipID(1)))
+
+
+    
+    
 
 
 
     
+    
+    
+
+
+
 
 
