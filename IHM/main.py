61a8f1cb67705783ed37cd2ae1d259f80fc5e3cf
Toh-Ma <92999833+Toh-Ma@users.noreply.github.com>
2022-04-02 17:17:55 +0200

    GetMapsStart

--- IHM/main.py
+++ IHM/main.py
@@ -1,7 +1,31 @@
 import pygame
+import sys
+import requests
+from 
+
+class Grille:
+    def __init__(self):
+        self
 
 
-class jeu:
+
+
+class Jeu:
     def __init__(self):
         self.ecran=pygame.display.set_mode(1000,700)
         pygame.display.set_caption('Space Travellers')
+        self.jeu_en_cours=True
+
+    def fonction_principale(self):
+        while self.jeu_en_cours:
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    sys.exit()
+
+if __name__ =='__main__':
+    pygame.init()
+    Jeu().fonction_principale()
+    pygame.quit()
+
+
+    
